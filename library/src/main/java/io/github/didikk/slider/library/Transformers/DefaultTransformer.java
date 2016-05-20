53aaffed301d31a03c697456dfe96954a8925798
didikk <ismawantodidik@gmail.com>
2016-05-20 09:48:23 +0700

    Initial commit

---
+++ library/src/main/java/io/github/didikk/slider/library/Transformers/DefaultTransformer.java
@@ -0,0 +1,16 @@
+package io.github.didikk.slider.library.Transformers;
+
+import android.view.View;
+
+public class DefaultTransformer extends BaseTransformer {
+
+	@Override
+	protected void onTransform(View view, float position) {
+	}
+
+	@Override
+	public boolean isPagingEnabled() {
+		return true;
+	}
+
+}
